a5c73bbae1633c06267483aea05e5be20e8f9ae4
gabriel-melo <gabrielmvalley@gmail.com>
2022-10-22 15:43:58 -0300

    Adding functionalities to tasks

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,76 @@ import { PlusCircle } from 'phosphor-react'
 import { Header } from './components/Header'
 
 import clipboard from './assets/clipboard.svg'
+import { Task } from './components/Task'
+import { ChangeEvent, FormEvent, useState } from 'react'
+
+export interface Tasks {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+}
 
 export function App() {
-  const tasks = [
-    
-  ]
+  const [tasks, setTasks] = useState<Tasks[]>([
+    {
+      id: 'string',
+      title: 'teste',
+      isCompleted: true,
+    },    
+  ])
+
+  const taskQuantity = tasks.length;
+  const completedTasks = tasks.filter((task) => task.isCompleted).length
+
+  const [title, setTitle] = useState("");
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    setTasks([...tasks, {
+      id: crypto.randomUUID(),
+      title: title,
+      isCompleted: false
+    }])
+
+    setTitle("")
+  }
+
+  function handleToggleCheckedStatus(taskId: string) {
+    const newTasks = tasks.map((task) => {
+      if(task.id === taskId) {
+        return {
+          ...task,
+          isCompleted: !task.isCompleted,
+        }
+      }
+      return task
+    })
+
+    setTasks(newTasks)
+  }
+
+  function handleChangeText(event: ChangeEvent<HTMLInputElement>) {
+    console.log(title)
+    setTitle(event.target.value)
+  }
+
+  function handleDeleteTask(taskId:string) {
+    const newTasks = tasks.filter((task) => task.id !== taskId)
+    setTasks(newTasks)
+  }
 
   return (
     <div>
       <Header />
 
       <div className={styles.wrapper}>
-        <form className={styles.formGroup}>
-          <input type="text" placeholder="Adicione uma nova tarefa" />
+        <form className={styles.formGroup} onSubmit={handleSubmit}>
+          <input
+            placeholder="Adicione uma nova tarefa"
+            onChange={handleChangeText} 
+            value={title}
+          />
           <button type="submit">
             Criar
             <PlusCircle size={20} />
@@ -27,16 +84,16 @@ export function App() {
         <div className={styles.taskCounter}>
           <div className={styles.createdTasks}>
             <strong>Tarefas criadas</strong>
-            <span>0</span>
+            <span>{taskQuantity}</span>
           </div>
 
           <div className={styles.completedTasks}>
             <strong>Concluídas</strong>
-            <span>0</span>
+            <span>{completedTasks} de {taskQuantity}</span>
           </div>
         </div>
 
-        {tasks.length === 0 ? (
+        {taskQuantity === 0 ? (
           <section className={styles.notFoundTasks}>
             <img src={clipboard} alt="Icone prancheta com lista" />
             <div className={styles.paragraph}>
@@ -45,7 +102,17 @@ export function App() {
             </div>
           </section>
         ) : (
-          <div></div>
+          <>
+            {tasks.map(task => {
+              return (
+              <Task
+                tasks={task}
+                onDelete={handleDeleteTask}
+                onComplete={handleToggleCheckedStatus}
+              />
+              )
+            })}
+          </>
         )}
       </div>
     </div>
